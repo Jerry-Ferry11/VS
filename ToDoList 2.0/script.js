e7e9c6b4b7d8f23749dad7b8a687129b0e6760b5
Jerry Ferry <jeremieidemudia@gmail.com>
2024-05-16 05:35:15 +0100

    A new version of the current ToDoList project

---
+++ ToDoList 2.0/script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('todo-form');
+const input = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const todoText = input.value.trim();
+    if (todoText !== '') {
+        addTodoItem(todoText);
+        input.value = '';
+    }
+});
+
+function addTodoItem(todoText) {
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <span>${todoText}</span>
+        <button class="edit-btn">Edit</button>
+        <button class="delete-btn">Delete</button>
+    `;
+    todoList.appendChild(li);
+
+    const deleteBtn = li.querySelector('.delete-btn');
+    deleteBtn.addEventListener('click', function() {
+        li.remove();
+    });
+}
